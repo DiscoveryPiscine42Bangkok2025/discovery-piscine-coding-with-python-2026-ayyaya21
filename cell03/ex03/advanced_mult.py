4da36fbd8ebe25122d4fa7dd1746c0c86089d5ec
ayyaya21 <66070221@kmitl.ac.th>
2026-02-11 14:11:07 +0700

    cell 3.3

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "yolo":
+        print("none")
+        sys.exit()
+    
+def generate_multiplication_tables():
+    for i in range(11):
+        print(f"Table de {i}:", end="")
+        for j in range(11):
+            print(f"{i * j:2}", end=" ")
+        print()
+
+generate_multiplication_tables()
