5ccf0b105de1070146868286cabaf9b96c40bd0a
ayyaya21 <66070221@kmitl.ac.th>
2026-02-11 16:23:43 +0700

    cell 5.2

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+original_arr =  [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_arr = []
+
+for i in range(len(original_arr)):
+    num = original_arr[i]+2
+    if num > 5:   
+        new_arr.append(num)
+
+print(f"Original array: {original_arr}")
+print(f"New array: {new_arr}")
