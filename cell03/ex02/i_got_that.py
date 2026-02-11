e067989f797f5e8acbe11edb11bf24ff3181f125
ayyaya21 <66070221@kmitl.ac.th>
2026-02-11 13:52:48 +0700

    cell 3.2

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+msg = input("What you gotta say? : ")
+
+while True:
+
+    if msg == "STOP":
+        break
+    else:
+        msg = input("I got that! Anything else? : ")
